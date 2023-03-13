cba2e6e9f8e83ee95c9383d51ab6a0674fe267b8
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-03-13 19:56:57 -0400

    Add free_postions and place_marker functions to the Game class

--- game.rb
+++ game.rb
@@ -3,6 +3,8 @@ module TicTacToe
 
     class Game
         def initialize(player_class_1, player_class_2)
+
+            # Board has 9 postions total, we will ignore index 0 for ease of use.
             @board = Array.new(10)
 
             @current_player_id = 0 
@@ -17,10 +19,10 @@ module TicTacToe
 
             puts @players[0].name, @players[1].name
         end
-        attr_reader :board, :current_player
+        attr_reader :board, :current_player_id 
 
         def current_player
-            @players[current_player]
+            @players[current_player_id]
         end 
 
         def other_player_id
@@ -31,6 +33,7 @@ module TicTacToe
             @current_player_id = other_player_id
         end
 
+        # This function prints the board accoarding to avialable spots on the board
         def print_board
             col_separator, row_separator = " | ", "--+---+--"
         
@@ -46,7 +49,54 @@ module TicTacToe
             puts rows_for_display.join("\n" +row_separator + "\n") 
         end
 
-        
+
+        # This function starts the game 
+        def play
+            loop do
+                place_player_marker(current_player)
+                
+                if player_has_won?(current_player)
+                  puts "#{current_player} wins!"
+                  print_board
+                  return
+                elsif board_full?
+                  puts "It's a draw."
+                  print_board
+                  return
+                end
+                
+                switch_players!
+            end
+        end 
+
+        def free_positions
+            (1..9).select {|position| @board[position].nil?}
+        end
+
+        def place_player_marker(player)
+            selection = current_player.select_postion
+
+            puts "#{player.name} places a #{player.marker} on position #{select_postion}"
+
+            @board[selection] = current_player.marker
+
+            print_board
+        end
+
+        # Check if there are any open positions left
+        def board_full?
+            free_positions.len == 0? true : false
+        end 
+
+        def player_has_won?(player)
+            # Check if any winning line...
+            LINES.any? do |line|
+                #...has all postions when compared to the board owned by the same player 
+                line.all? {|positon| @board[position] == player.marker}
+            end 
+        end 
+
+
     
     end
 end 
